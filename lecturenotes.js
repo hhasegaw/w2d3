602d485cb5365a586b13f29dbc7bccdac0b14ca3
hanna hasegawa <noemail@noemail.com>
2018-09-05 10:04:15 -0400

    first commit

---
+++ lecturenotes.js
@@ -0,0 +1,23 @@
+// curd
+//rest representative state  --- min amount of information
+// put means change
+// lps -- long play
+// morgan is login library
+
+
+// get /lps
+//get /lps/year/1966 --- aceess to 1966
+// get /lps/artist/miles
+//get / lps/kind-of-blue
+
+
+//post/lps  --- adding new 
+// {playload}
+// put /lps/kind-of-blue 
+// {playload}
+// delete /lps/kind-of-blue
+
+
+// res.send("") --send respond to brawser
+//app.get(`/lps`)
+//res.render(`show`, read('year', req.params.year))
